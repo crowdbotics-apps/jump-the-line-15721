8e80d2308ec5614e76001b5a7c50a05e0db2330d
Crowdbotics <team@crowdbotics.com>
2020-04-10 23:00:12 +0000

    Initial commit.

---
+++ jump_the_line_15721/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jump_the_line_15721 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jump_the_line_15721.settings')
+
+application = get_wsgi_application()
